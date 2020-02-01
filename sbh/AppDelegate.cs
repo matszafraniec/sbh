991a87790d70e66db5c8d61a92f1a5f727475092
Mateusz Szafraniec <mat.szafraniec@outlook.com>
2020-02-01 16:03:39 +0100

    configuring Pushwoosh.

--- sbh/AppDelegate.cs
+++ sbh/AppDelegate.cs
@@ -1,12 +1,15 @@
 ﻿using Foundation;
 using UIKit;
+using Pushwoosh;
+using UserNotifications;
+using System;
 
 namespace sbh
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the
     // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
     [Register("AppDelegate")]
-    public class AppDelegate : UIResponder, IUIApplicationDelegate
+    public class AppDelegate : UIApplicationDelegate, IUIApplicationDelegate
     {
 
         [Export("window")]
@@ -17,6 +20,20 @@ namespace sbh
         {
             // Override point for customization after application launch.
             // If not required for your application you can safely delete this method
+
+            PushNotificationManager pushmanager = PushNotificationManager.PushManager;
+            pushmanager.Delegate = new PushDelegate();
+            UNUserNotificationCenter.Current.Delegate = pushmanager.NotificationCenterDelegate;
+
+            if (launchOptions != null)
+            {
+                if (launchOptions.ContainsKey(UIApplication.LaunchOptionsRemoteNotificationKey))
+                {
+                    pushmanager.HandlePushReceived(launchOptions);
+                }
+            }
+
+            pushmanager.RegisterForPushNotifications();
 
             return true;
         }
@@ -37,6 +54,44 @@ namespace sbh
             // Called when the user discards a scene session.
             // If any sessions were discarded while the application was not running, this will be called shortly after `FinishedLaunching`.
             // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            PushNotificationManager.PushManager.HandlePushRegistration(deviceToken);
+        }
+
+        public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+        {
+            PushNotificationManager.PushManager.HandlePushRegistrationFailure(error);
+        }
+
+        public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+        {
+            PushNotificationManager.PushManager.HandlePushReceived(userInfo);
+        }
+    }
+
+    public class PushDelegate : PushNotificationDelegate
+    {
+        public override void OnPushAccepted(PushNotificationManager pushManager, NSDictionary pushNotification)
+        {
+            Console.WriteLine("Push accepted: " + pushNotification);
+        }
+
+        public override void OnPushReceived(PushNotificationManager pushManager, NSDictionary pushNotification, bool onStart)
+        {
+            Console.WriteLine("Push received: " + pushNotification);
+        }
+
+        public override void OnDidRegisterForRemoteNotificationsWithDeviceToken(NSString token)
+        {
+            Console.WriteLine("Registered for push notifications: " + token);
+        }
+
+        public override void OnDidFailToRegisterForRemoteNotificationsWithError(NSError error)
+        {
+            Console.WriteLine("Error: " + error);
         }
     }
 }
